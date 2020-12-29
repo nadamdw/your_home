f00ca3b96bc7ecaa62bdf62b8114fa77b9892190
nadamdw <200916nada>
2020-12-29 15:35:47 +0100

    update

--- app/src/main/java/com/example/yourhome/MainActivity.java
+++ app/src/main/java/com/example/yourhome/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.yourhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+ Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn =(Button)findViewById(R.id.sign_in);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                homePage();
+            }
+        });
+    }
+    public void homePage(){
+        Intent intent=new Intent(this,home_pageAct.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/yourhome/home_pageAct.java
@@ -0,0 +1,20 @@
+package com.example.yourhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class home_pageAct extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+
+    }
+
+}
